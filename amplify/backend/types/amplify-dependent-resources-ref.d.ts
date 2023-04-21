199d789951a514cc987f3802bf6aa228faca806a
Harjot Singh <hsbinsardari12g@outlook.com>
2023-04-21 18:29:26 -0400

    added graphql and storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "shoppinglist2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "shoppinglist289899add": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "Harjot": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
